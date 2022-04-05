ca348a91d2b7170aa85245ac4ade0196e8f87566
Rachel Kiffiak <rachelkiffiak1@gmail.com>
2022-04-04 21:04:08 -0600

    finished the Blog post app.

--- app.js
+++ app.js
@@ -1,26 +1,50 @@
+
+let postsArray = []
+const form = document.getElementById("new-post")
+
+function renderPosts() {
+    let html = ""
+        for (let post of postsArray) {
+            html += `
+                <h3>${post.title}</h3>
+                <p>${post.body}</p>
+                <hr />
+            `
+        }
+        document.getElementById("post").innerHTML = html
+}
+
 fetch("https://apis.scrimba.com/jsonplaceholder/posts")
     .then(res => res.json())
     .then(data => {
-        const postsArr = data.slice(0, 5)
-        let html = ""
-        for (post of postsArr) {
-            html += `
-            <h3>${post.title}</h3>
-            <p>${post.body}</p>
-            <hr />`
-        }
-        document.getElementById("post").innerHTML = html
-       
-        
+        postsArray = data.slice(0, 5)
+        renderPosts()
     })
 
-    document.getElementById("new-post").addEventListener("submit", function(e){
-        e.preventDefault()
-        const postTitle = document.getElementById("post-title").value
-        const postBody = document.getElementById("post-body").value
-        const data = {
-           title: postTitle,
-           body: postBody
+form.addEventListener("submit", function(e) {
+    e.preventDefault()
+    const postTitle = document.getElementById("post-title").value
+    const postBody = document.getElementById("post-body").value
+    const data = {
+        title: postTitle,
+        body: postBody
+    }
+    
+    const options = {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json"
         }
-        console.log(data)
-    })
+    }
+    
+    fetch("https://apis.scrimba.com/jsonplaceholder/posts", options)
+        .then(res => res.json())
+        .then(post => {
+           postsArray.unshift(post)
+           renderPosts()
+           form.reset()
+        })
+})
+
+   
